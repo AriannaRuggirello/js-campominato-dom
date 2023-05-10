dd5b422ddac7895edcb3871fd3a3fa560d6352e4
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-10 21:34:55 +0200

    bonus

--- js/main.js
+++ js/main.js
@@ -15,69 +15,27 @@ let punteggio = 0;
 let gridElement = document.querySelector('div.grid')
 // L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 button.addEventListener("click", 
-    function Game()
-    {   
-        // reset dei numeri selezionati in pagina
-       gridElement.innerHTML = ''
-            
-        // Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-        //array vuoto
-       let bombArr = [];
-
-        // Finché l'array non ha 16 elementi, genera numeri casuali unici tra 1 e 100 e li aggiunge all'array
-        while (bombArr.length < 16) {
-            let randomNum = Math.floor(Math.random() * 100) + 1;
-            // Se il numero non è già presente nell'array, lo aggiungo
-            if (!bombArr.includes(randomNum)) {
-                bombArr.push(randomNum);
-            }
-            }
+    function(){
+        // selezione il select
+        let livelloSelezionato = document.getElementById('levels').value;
+        // var 
+        let easyGame;
+        let mediumGame;
+        let hardGame;
     
-            console.log(bombArr);
-
-        // Ogni cella ha un numero progressivo, da 1 a 100.
-        for(let i = 1; i <= 100 ; i++){
-
-        // creare l'elemento quadrato all'interno della griglia
-            const newSquare = createGridSquare('div','square-easy'); 
-
-        // inserisco dentro alla griglia i quadratini
-            gridElement.append(newSquare);
-
-        // numero ogni quadratino
-            newSquare.innerHTML =i
-            
-        // Quando l’utente clicca sulla cella
-            newSquare.addEventListener('click', 
-            function(){   
-            // incremento puntegio
-                punteggio++;
-            
-                if(bombArr.includes(i)){
-                      // se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
-                    newSquare.classList.add('clicked-false');
-                    // emetto un messaggio in console con il numero della cella cliccata.
-                    alert('BOOM! Hai perso!' + ' Hai totalizzato: ' + punteggio + ' punti ')
-                    // reset dopo l'alert
-                    gridElement.innerHTML = ''
-                    // punteggio riparte da zero
-                    punteggio = 0;
-
-                }
-                else{
-                    // Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle
-                    newSquare.classList.add('clicked-true');
-                    
-                    console.log(punteggio);
-                    
-                   
-                }
-            })
+        if(livelloSelezionato === 'Easy'){
+            easyGame = createGameFunction(100,16,'div','square-easy');
         }
-    });
+        if(livelloSelezionato === 'Medium'){
+            mediumGame = createGameFunction(81,16,'div','square-medium');
+    
+        }
+        if(livelloSelezionato === 'Hard'){
+            hardGame = createGameFunction(49,16,'div','square-hard');
+            
+        }
 
-
-
+    })
 
 
     // FUNZIONI
@@ -88,3 +46,79 @@ button.addEventListener("click",
         return newElement;
         
     }
+
+    function createRandomArray (max,numMaxElementArr){
+       //array vuoto
+     let intArr = [];
+
+     // Finché l'array non ha raggiunto il numero massimo di elementi , genera numeri casuali unici tra 1 e un numero massimo e li aggiunge all'array
+     while (intArr.length < numMaxElementArr) {
+         let randomNum = Math.floor(Math.random() * max) + 1;
+         // Se il numero non è già presente nell'array, lo aggiungo
+         if (!intArr.includes(randomNum)) {
+             intArr.push(randomNum);
+         }
+         }
+ 
+         return intArr;
+    }
+
+
+    function createGameFunction(max,numMaxElementArr,tagType,classToAdd){
+// reset dei numeri selezionati in pagina
+gridElement.innerHTML = ''
+         
+// ARRAY 
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+let bombArr = createRandomArray (max,numMaxElementArr);
+console.log(bombArr);
+
+
+// FOR
+// Ogni cella ha un numero progressivo, da 1 a 100.
+for(let i = 1; i <= max ; i++)
+{
+    // creare l'elemento quadrato all'interno della griglia
+    const newSquare = createGridSquare(tagType,classToAdd); 
+
+    // inserisco dentro alla griglia i quadratini
+    gridElement.append(newSquare);
+
+    // numero ogni quadratino
+    newSquare.innerHTML = i
+    
+    // Quando l’utente clicca sulla cella
+    newSquare.addEventListener('click', 
+    function(){   
+    // incremento puntegio
+    punteggio++;
+    let userScore = document.getElementById('user-score');
+
+        // CONDIZIONE IF/ELSE
+            if(bombArr.includes(i))
+            {
+                // se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+                newSquare.classList.add('clicked-false');
+                // emetto un messaggio in console con il numero della cella cliccata.
+                alert('BOOM! Hai perso!' + ' Hai totalizzato: ' + (punteggio - 1) + ' punti ')
+            
+                // punteggio riparte da zero
+                punteggio = 0;
+                userScore = punteggio;
+                // reset dopo l'alert
+                gridElement.innerHTML = ''
+            }
+            else
+            {
+                // Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle
+                newSquare.classList.add('clicked-true');
+                
+                userScore.innerHTML = punteggio
+                console.log(punteggio);
+                
+            
+            }
+        })
+}
+    }
+    
